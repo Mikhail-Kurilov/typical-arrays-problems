d67f6bd529e6dc3077d81c3d9962c4a2c75df4a8
Mikhail-Kurilov <mvkurilov@mail.ru>
2020-10-03 20:28:39 +0400

    init: create tusk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (array && array.length) {
+    return Math.min(...array);
+  }
   return 0;
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (array && array.length) {
+    return Math.max(...array);
+  }
   return 0;
 }
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (array && array.length) {
+    let x = array.length;
+    return array.reduce(function (a, b) { return a + b }, 0) / x;
+  }
   return 0;
 }
